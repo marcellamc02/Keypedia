0a610fbe4ac45b7e7be3f93e592502a1f4a2aad7
StefannySusilo <fannysusilo23@gmail.com>
2022-01-05 22:57:53 +0700

    Master view and Login view

---
+++ keypedia/resources/views/login.blade.php
@@ -0,0 +1,32 @@
+@extends('loginregister-master')
+
+@section('title') Login | Keypedia @endsection
+
+@section('content')
+
+<div class="container main d-flex justify-content-center align-items-center">
+    <div class="form-container ">
+        <p class="fw-bold fs-4">Login</p>
+        <hr>
+        <form>
+            @csrf
+            <div class="mb-3">
+              <label for="email-text" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+              <label for="password-text" class="form-label">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3 form-check remember-me">
+              <input type="checkbox" class="form-check-input" id="exampleCheck1">
+              <label class="form-check-label" for="exampleCheck1">Remember me</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+          </form>
+
+    </div>
+</div>
+
+
+@endsection
